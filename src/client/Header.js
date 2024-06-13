ccbfdc924aafde8542e65e9fc15732f780bf2d27
Sanchay-7 <124687431+Sanchay-7@users.noreply.github.com>
2024-06-13 17:00:09 +0530

    added header,footer and Home page files

---
+++ src/client/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function UserHeader() {
+    return (
+        <>
+            <header>
+                {/* Header content goes here */}
+            </header>
+        </>
+    );
+}
+
+export default UserHeader;
---
+++ src/client/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import UserHeader from './UserHeader';
+import UserFooter from './UserFooter';
+
+function UserHome() {
+    return (
+        <>
+            <UserHeader />
+
+
+            <UserFooter />
+        </>
+    );
+}
+
+export default UserHome;
